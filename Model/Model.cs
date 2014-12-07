0db778c76c8f71aabf380fdb98db32a314d111ee
Ron Parker <rdparker@gmail.com>
2014-12-07 15:21:17 -0500

    Initial Commit
    This is in the process of making changes to the implementation.  Specifically after wrapping the setter with notification logic changes are needed to support non-reference types such as int.

---
+++ Model/Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Model
+    {
+        int _number;
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+    }
+}
---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ViewModel
+{
+    using Binding;
+    using Model;
+
+    public class ViewModel : ObserableObject
+    {
+        Model _model = new Model();
+
+        public ViewModel()
+        {
+            Func<int> numberGetter = () => { return Number; };
+            Action<int> numberSetter = (x) => Number = x;
+
+            RegisterProperty("Number", () => { return Number; }, (x) => Number = x);
+            RegisterProperty("Text", () => { return Text; }, (x) => Text = x );
+            RegisterProperty("Computed", () => { return Computed; });
+        }
+
+        public int Number
+        {
+            get { return _model.Number; }
+            set { _model.Number = value; }
+        }
+
+        public string Text
+        {
+            get { return _model.Text; }
+            set { _model.Text = value; }
+        }
+
+        public string Computed {
+            get { return _model.Text + ": " + _model.Number; }
+        }
+    }
+}
